df4c4a6f4448c0b43499ce861dde00756dde8885
Gerrit Meier <meistermeier@gmail.com>
2019-06-12 10:53:40 +0200

    Remove licence header from package-info files.

--- spring-data-neo4j-rx/src/main/java/org/neo4j/springframework/data/repository/support/package-info.java
+++ spring-data-neo4j-rx/src/main/java/org/neo4j/springframework/data/repository/support/package-info.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2019 "Neo4j,"
- * Neo4j Sweden AB [https://neo4j.com]
- *
- * This file is part of Neo4j.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /**
  * This package contains infrastructure code that set up the needed Spring beans to register the repository
  * capabilities. Additional it holds the implementations of the {@link org.springframework.data.repository.PagingAndSortingRepository}
