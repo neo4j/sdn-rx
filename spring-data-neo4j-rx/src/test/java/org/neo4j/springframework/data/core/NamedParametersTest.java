60b04ae2f6c7808ec836f3fc4094a83110ee2517
Gerrit Meier <meistermeier@gmail.com>
2019-08-09 12:09:08 +0200

    Polishing.

--- spring-data-neo4j-rx/src/test/java/org/neo4j/springframework/data/core/NamedParametersTest.java
+++ spring-data-neo4j-rx/src/test/java/org/neo4j/springframework/data/core/NamedParametersTest.java
@@ -22,7 +22,6 @@ import static java.util.Collections.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
