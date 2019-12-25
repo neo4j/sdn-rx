f3ab93176e007ceced6cab8b4658b3c59d0f0349
Gerrit Meier <meistermeier@gmail.com>
2019-12-25 20:50:12 +0100

    GH-102 - Eagerly create related entities first.
    This will change the mapping behaviour to map related entities
    before creating the "owning" entity.
    As a side-effect the logic for creating the related entities
    got extracted to avoid code duplication.

---
+++ spring-data-neo4j-rx/src/test/kotlin/org/neo4j/springframework/data/integration/imperative/ImmutableRelationshipsIT.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.neo4j.springframework.data.integration.imperative
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.Driver
+import org.neo4j.driver.GraphDatabase
+import org.neo4j.springframework.data.config.AbstractNeo4jConfig
+import org.neo4j.springframework.data.core.schema.GeneratedValue
+import org.neo4j.springframework.data.core.schema.Id
+import org.neo4j.springframework.data.core.schema.Node
+import org.neo4j.springframework.data.core.schema.Relationship
+import org.neo4j.springframework.data.repository.Neo4jRepository
+import org.neo4j.springframework.data.repository.config.EnableNeo4jRepositories
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+/**
+ * This test originate from https://github.com/neo4j/sdn-rx/issues/102.
+ * It is designed to ensure the capability of creating dependent relationships for immutable objects before
+ * the creation of the object itself.
+ *
+ * @author Gerrit Meier
+ */
+@ExtendWith(SpringExtension::class)
+class ImmutableRelationshipsIT @Autowired constructor(
+    private val repository: DeviceRepository,
+    private val driver: Driver
+) {
+    @Test
+    fun createRelationshipsBeforeRootObject() {
+
+        driver.session().use { session ->
+            session.run("MATCH (n) DETACH DELETE n")
+            session.run("CREATE (n:DeviceEntity {deviceId:'123', phoneNumber:'some number'})-[:LATEST_LOCATION]->(l1: LocationEntity{latitude: 20.0, longitude: 20.0})")
+        }
+        val device = repository.findById("123").get()
+        assertThat(device.deviceId).isEqualTo("123")
+        assertThat(device.phoneNumber).isEqualTo("some number")
+
+        assertThat(device.location).isNotNull
+        assertThat(device.location!!.latitude).isEqualTo(20.0)
+        assertThat(device.location!!.longitude).isEqualTo(20.0)
+    }
+
+    @Configuration
+    @EnableTransactionManagement
+    @EnableNeo4jRepositories
+    open class MyConfig : AbstractNeo4jConfig() {
+        @Bean
+        override fun driver(): Driver {
+            return GraphDatabase.driver("neo4j://localhost:7687", AuthTokens.basic("neo4j", "secret"))
+        }
+
+        override fun getMappingBasePackages(): Collection<String> {
+            return listOf(ImmutableRelationshipsIT::class.java.getPackage().name)
+        }
+
+    }
+
+}
+
+interface DeviceRepository: Neo4jRepository<DeviceEntity, String>
+
+@Node
+data class DeviceEntity(
+    @Id
+    val deviceId: String,
+    val phoneNumber: String,
+    @Relationship(type = "LATEST_LOCATION", direction = Relationship.Direction.OUTGOING)
+    val location: LocationEntity?
+)
+
+@Node
+data class LocationEntity(
+    @Id
+    @GeneratedValue
+    val locationId: Long? = null,
+    val latitude: Double,
+    val longitude: Double,
+    @Relationship(type = "PREVIOUS_LOCATION", direction = Relationship.Direction.OUTGOING)
+    val previousLocation: LocationEntity?
+)
+
