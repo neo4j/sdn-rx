d08ff954fe67539493e5fb6a88505fee95d3823f
Gerrit Meier <meistermeier@gmail.com>
2020-01-09 16:08:52 +0100

    GH-111 - Add test for reactive part.

--- spring-data-neo4j-rx/src/test/java/org/neo4j/springframework/data/integration/reactive/ReactiveRepositoryIT.java
+++ spring-data-neo4j-rx/src/test/java/org/neo4j/springframework/data/integration/reactive/ReactiveRepositoryIT.java
@@ -36,7 +36,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.assertj.core.data.MapEntry;
