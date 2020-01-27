edd4f1d8491909009d730e051490796c815f4b43
Michael Simons <michael.simons@neo4j.com>
2020-01-27 20:46:45 +0100

    Add examples.

---
+++ examples/multi-database/src/main/java/org/neo4j/springframework/data/examples/spring_boot/Neo4jConfig.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.springframework.data.examples.spring_boot;
+
+import java.util.Optional;
+
+import org.neo4j.springframework.data.core.Neo4jDatabaseNameProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+/**
+ * @author Michael J. Simons
+ */
+@Configuration
+public class Neo4jConfig {
+
+	@Bean
+	@Profile("selection-by-user")
+	Neo4jDatabaseNameProvider databaseNameProvider() {
+
+		return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+			.map(SecurityContext::getAuthentication)
+			.filter(Authentication::isAuthenticated)
+			.map(Authentication::getPrincipal)
+			.map(User.class::cast)
+			.map(User::getUsername);
+	}
+}
