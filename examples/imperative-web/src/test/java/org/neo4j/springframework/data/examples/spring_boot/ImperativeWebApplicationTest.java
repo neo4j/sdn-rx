cf8ccb53a9772c4e96335ed6c1a0b2de3b7fd7cf
Michael Simons <michael.simons@neo4j.com>
2020-05-19 17:21:10 +0200

    Use AutoConfigureMockMvc.

--- examples/imperative-web/src/test/java/org/neo4j/springframework/data/examples/spring_boot/ImperativeWebApplicationTest.java
+++ examples/imperative-web/src/test/java/org/neo4j/springframework/data/examples/spring_boot/ImperativeWebApplicationTest.java
@@ -32,30 +32,26 @@ import org.junit.jupiter.api.Test;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @author Gerrit Meier
  */
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ImperativeWebApplicationTest {
 
 	@Autowired
 	private Driver driver;
 
 	@Autowired
-	private WebApplicationContext wac;
-
 	private MockMvc client;
 
 	@BeforeEach
 	void setupData() throws IOException {
-		client = MockMvcBuilders.webAppContextSetup(wac).build();
-
 		try (BufferedReader moviesReader = new BufferedReader(
 			new InputStreamReader(this.getClass().getResourceAsStream("/movies.cypher")));
 			Session session = driver.session()) {
@@ -67,7 +63,6 @@ public class ImperativeWebApplicationTest {
 				return null;
 			});
 		}
-
 	}
 
 	@Test
